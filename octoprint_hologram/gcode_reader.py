8aec969b84a3ab271823bba1854e9d97cd83db2e
Mark <m.slattery13@nuigalway.ie>
2024-05-28 14:45:44 +0100

    Removed seaborn dependency

--- octoprint_hologram/gcode_reader.py
+++ octoprint_hologram/gcode_reader.py
@@ -34,7 +34,7 @@ import matplotlib.pyplot as plt
 import matplotlib.animation as manimation
 from mpl_toolkits.mplot3d import Axes3D
 import pandas as pd
-import seaborn as sns
+# import seaborn as sns
 
 # sns.set()  # use seaborn style
 
